6cb5db29892a8927a63245e64af1090790b24ef7
dev <suport@hubbix.com.br>
2026-07-09 16:33:35 -0300

    Updt: Updated history

--- models/rp_historico.py
+++ models/rp_historico.py
@@ -16,16 +16,3 @@ class History(BaseModel):
     ended_at = db.Column(db.DateTime)
     motivo = db.Column(db.String)
     obs = db.Column(db.String)
-    
-class Requisicao(BaseModel):
-    __tablename__ = "rp_requisicoes"
-
-    id = db.Column(db.Integer, primary_key=True)
-    reserva_id = db.Column(db.Integer, nullable=False)
-    ausente_id = db.Column(db.Integer, nullable=False)
-    cc = db.Column(db.Integer, nullable=True)
-    supervisor_id = db.Column(db.Integer, nullable=False)
-    warning = db.Column(db.Boolean, default=False)
-    motivo = db.Column(db.String)
-    obs = db.Column(db.String)
-    created_at = db.Column(db.DateTime, default=dt.now())
