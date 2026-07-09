e0ee8219031029fa56f346da376a668938326ab3
dev <suport@hubbix.com.br>
2026-07-09 16:33:55 -0300

    Updt: Changed namefiles for requests and repos

---
+++ models/rp_requisicao.py
@@ -0,0 +1,17 @@
+from models.base_model import BaseModel
+from datetime import datetime as dt
+from utils.db import db
+
+class Requisicao(BaseModel):
+    __tablename__ = "rp_requisicoes"
+
+    id = db.Column(db.Integer, primary_key=True)
+    reserva_id = db.Column(db.Integer, nullable=False)
+    ausente_id = db.Column(db.Integer, nullable=False)
+    cc = db.Column(db.Integer, nullable=True)
+    supervisor_id = db.Column(db.Integer, nullable=False)
+    warning = db.Column(db.Boolean, default=False)
+    motivo = db.Column(db.String)
+    obs = db.Column(db.String)
+    created_at = db.Column(db.DateTime, default=dt.now())
+    status = db.Column(db.String, default="pending")
