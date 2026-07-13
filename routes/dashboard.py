54c6dccde4c12ea85942c6b346e22a6739e2f997
dev <suport@hubbix.com.br>
2026-07-13 09:34:06 -0300

    Added routes for employees from dptos. Dashboard

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -6,3 +6,6 @@ service = DashboardService()
 
 @dashboards_bp.route("/reposicoes", methods=["POST"])
 def root(): return service.get_repos()
+
+@dashboards_bp.route("/colaboradores-departamento", methods=["POST"])
+def employees_by_department(): return service.get_employees_by_department()
