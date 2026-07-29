ce3d2f67fd07f198dc8ce3638eae486a2092a45d
Guilherme Breve <foxtec198@gmail.com>
2026-07-29 11:11:32 -0300

    Support chunked collaborator imports

--- routes/importacao_colaboradores.py
+++ routes/importacao_colaboradores.py
@@ -12,6 +12,21 @@ def create_import():
     return service.create()
 
 
+@collaborator_import_bp.post("/upload/iniciar")
+def start_import_upload():
+    return service.start_upload()
+
+
+@collaborator_import_bp.post("/<string:job_id>/parte")
+def upload_import_chunk(job_id):
+    return service.upload_chunk(job_id)
+
+
+@collaborator_import_bp.post("/<string:job_id>/concluir")
+def complete_import_upload(job_id):
+    return service.complete_upload(job_id)
+
+
 @collaborator_import_bp.get("/<string:job_id>")
 def import_status(job_id):
     return service.read(job_id)
