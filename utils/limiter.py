0edd5c78c0da1495542b147ec95165d31295c233
Guilherme Breve <64221923+foxtec198@users.noreply.github.com>
2026-09-14 08:54:37 -0300

    Changed options for limiter, now limits by username

--- utils/limiter.py
+++ utils/limiter.py
@@ -1,9 +1,14 @@
 from os import getenv
-from flask_limiter.util import get_remote_address
+from flask import request
 from flask_limiter import Limiter
 
+# Função responsavel por gerar um id usando username
+def get_rate_limit_key():
+    user_id = request.get_json(silent=True).get("username")
+    return f"user:{user_id}"
+
 limiter = Limiter(
-    key_func=get_remote_address,
+    key_func=get_rate_limit_key,
     storage_uri=getenv("RATELIMIT_STORAGE_URI", "memory://"),
     default_limits=[],
 )
